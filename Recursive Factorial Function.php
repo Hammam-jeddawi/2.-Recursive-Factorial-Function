a85eead38545506f5072fbcbb6c33a2169122d70
Hammam-jeddawi <hammam.f.aljeddawi@outlook.com>
2024-06-25 06:12:40 +0300

    Update Recursive Factorial Function.php
    @param int $n The integer for which the factorial is to be calculated.
     
    @return int The factorial of the given integer.
    
     @throws InvalidArgumentException If the input is not a non-negative integer.

--- Recursive Factorial Function.php
+++ Recursive Factorial Function.php
@@ -8,10 +8,6 @@
     <?php
 
 
-// @param int $n The integer for which the factorial is to be calculated.
-// @return int The factorial of the given integer.
-// @throws InvalidArgumentException If the input is not a non-negative integer.
-
 function factorial(int $n): int {
    if ($n < 0) {
        throw new InvalidArgumentException('Factorial is not defined for negative numbers.');
